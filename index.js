a5d132d5425832df0cd780f96dfa0571b15bff67
Francesco Bambina <francescobambina@MacBook-Pro-di-Francesco.local>
2023-10-27 14:22:14 +0200

    Added first hints of style

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     function addBookToLibrary() {
         
     }
+
+    function displayLibrary() {
+        let parentNode = document.querySelector('#libraryContainer')
+        for (let i = 0; i < myLibrary.length; i++) {
+            // To be implemented, I still need to nail down the look.
+            childNode = 0;
+        }
+    }
 })
